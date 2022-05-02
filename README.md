bd292a91e943686ed5a7801b1b1402623b9ebece
Hamed Montazeri <hamed.mon@gmail.com>
2022-05-02 14:57:02 +0200

    init

--- README.md
+++ README.md
@@ -2,19 +2,22 @@
 
 Download all your Stripe PDF invoices in bulk.
 
+This project is a fork of `dunglas/stripe-invoice-exporter`
+
 # Prerequisites
 
-You need a working installation of [PHP](https://php.net) and of [Composer](https://getcomposer.org/).
+You need a working installation of node.
 
-## Install
+# Install
 
-1. Run `composer install` to install the Stripe SDK.
-2. Create a new restricted key with the `Read` right for `Invoices` resource type:
-    ![Screenshot](docs/restricted-key.png)
-3. Copy the generated key.
+1. Run `npm install` to install the Stripe SDK.
+2. Export the invoice ids from the Stripe Dashboard as CSV. Remove all columns but `id`.
+3. Create a new restricted key with the `Read` right for `Invoices` resource type:
+   ![Screenshot](docs/restricted-key.png)
+4. Copy the generated key.
 
-## Usage
+# Usage
 
-    STRIPE_KEY=rk_live_<...> ./download.php
+    STRIPE_KEY=rk_live_<...> node download.js
 
 The invoices will be downloaded in the `invoices/` directory.
---
+++ download.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const https = require("https");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const loadedInvoices = fs.readFileSync("./invoices.csv", "utf8");
+const invoices = loadedInvoices.split("\n");
+
+const getPdf = async (invoice) => {
+  const pdf = await stripe.invoices
+    .retrieve(invoice)
+    .then((data) => {
+      return data.invoice_pdf;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return pdf;
+};
+
+invoices.forEach(async (invoice, index) => {
+  const pdf = await getPdf(invoice);
+  const pdfFile = `./invoices/${invoice}.pdf`;
+  setTimeout(async () => {
+    await download(pdf, pdfFile);
+  }, 300 * index);
+});
+
+/**
+ * Download a resource from `url` to `dest`.
+ * @param {string} url - Valid URL to attempt download of resource
+ * @param {string} dest - Valid path to save the file.
+ * @returns {Promise<void>} - Returns asynchronously when successfully completed download
+ */
+function download(url, dest) {
+  return new Promise((resolve, reject) => {
+    // Check file does not exist yet before hitting network
+    fs.access(dest, fs.constants.F_OK, (err) => {
+      if (err === null) reject("File already exists");
+
+      const request = https.get(url, (response) => {
+        if (response.statusCode === 200) {
+          const file = fs.createWriteStream(dest, { flags: "wx" });
+          file.on("finish", () => resolve());
+          file.on("error", (err) => {
+            file.close();
+            if (err.code === "EEXIST") reject("File already exists");
+            else fs.unlink(dest, () => reject(err.message)); // Delete temp file
+          });
+          response.pipe(file);
+        } else if (response.statusCode === 302 || response.statusCode === 301) {
+          //Recursively follow redirects, only a 200 will resolve.
+          download(response.headers.location, dest).then(() => resolve());
+        } else {
+          reject(
+            `Server responded with ${response.statusCode}: ${response.statusMessage}`
+          );
+        }
+      });
+
+      request.on("error", (err) => {
+        reject(err.message);
+      });
+    });
+  });
+}
